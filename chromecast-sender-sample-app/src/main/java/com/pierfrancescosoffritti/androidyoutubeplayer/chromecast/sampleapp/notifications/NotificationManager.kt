47ee235cd0c89be3981158e3394d969889a6fd37
youssefrashed1999 <youssef.rashed1999@gmail.com>
2025-03-20 11:35:29 +0200

    Fix linting file

--- chromecast-sender-sample-app/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/chromecast/sampleapp/notifications/NotificationManager.kt
+++ chromecast-sender-sample-app/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/chromecast/sampleapp/notifications/NotificationManager.kt
@@ -81,10 +81,7 @@ class NotificationManager(
     notificationManager.cancel(notificationId)
   }
 
-  fun dismissNotification() {
-    val notificationManager = NotificationManagerCompat.from(context)
-    notificationManager.cancel(notificationId)
-  }
+  fun dismissNotification() = NotificationManagerCompat.from(context).cancel(notificationId)
 
   @Suppress("DEPRECATION")
   @SuppressLint("SwitchIntDef", "RestrictedApi")
